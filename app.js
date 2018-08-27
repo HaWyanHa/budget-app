31e483ded09af888d7f9f35324bf53a02a4835fe
Ryan Ha <ryan@hugsbyha.com>
2018-08-27 11:06:29 -0400

    added CSS change on type

--- app.js
+++ app.js
@@ -177,7 +177,41 @@ var UIController = (function() {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
+	};
+
+	var formatNumber = function(num, type) {
+			var numSplit, int, dec;
+
+			/*
+			+ or - before number
+			exactly 2 decimal points
+			comma seperating the thousands
+			2310.4567 -> + 2,310.46
+			2000 -> + 2,000.00
+			*/
+
+			num = Math.abs(num);
+			num = num.toFixed(2); //not a method of the Math object, but instead a mathod of the 'number' prototype
+			numSplit = num.split('.');
+
+			int = numSplit[0];
+
+			if (int.length > 3) {
+				int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);  //
+			}
+
+			dec = numSplit[1];
+
+			return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+
+		};
+
+	var nodeListForEach = function (list, callback) {
+		for (var i = 0; i < list.length; i++) {
+					callback(list[i], i);
+		}
 	};
 
 	return {
@@ -214,7 +248,7 @@ var UIController = (function() {
 			}
 			//Replace the placeholder text with actual data
 			newHtml = html.replace('%description%', obj.description);
-			newHtml = newHtml.replace('%value%', obj.value);
+			newHtml = newHtml.replace('%value%', formatNumber(obj.value));
 			newHtml = newHtml.replace('%id%', obj.id);
 
 			//Insert the HTML into the DOM
@@ -244,9 +278,12 @@ var UIController = (function() {
 		},
 
 		displayBudget: function(obj) {
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			var type;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 			if (obj.percentage > 0) {
 				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
@@ -259,11 +296,6 @@ var UIController = (function() {
 			
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 			
-			var nodeListForEach = function (list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
 
 			nodeListForEach(fields, function(current, index){
 				
@@ -275,6 +307,32 @@ var UIController = (function() {
 			});
 
 
+		},
+
+		displayMonth: function() {
+			var now, year, month;
+
+			now = new Date();
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			month = now.getMonth();
+			year = now.getFullYear();
+
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+
+		},
+
+		changeType: function() {
+
+			var fields = document.querySelectorAll(
+					DOMstrings.inputType + ',' +
+					DOMstrings.inputDescription + ',' +
+					DOMstrings.inputValue);
+
+
+			nodeListForEach(fields, function(cur){
+				cur.classList.toggle('red-focus');
+			});
 		},
 
 		getDOMstrings: function() {
@@ -300,6 +358,8 @@ var controller = (function(budgetCtrl, UICtrl) {   //pass the two arguments in h
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem); 
+
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changeType);
 	};
 
 	var updateBudget = function() {
@@ -373,6 +433,7 @@ var controller = (function(budgetCtrl, UICtrl) {   //pass the two arguments in h
 
 	return {
 		init: function(){
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
